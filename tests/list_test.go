f7b5de67d7722dcd1132222cfa313e292bbbba39
Rishi Desai <desai.rishi1@gmail.com>
2018-05-05 19:02:25 -0500

    added list remove test

--- tests/list_test.go
+++ tests/list_test.go
@@ -22,3 +22,22 @@ func TestAppend(t *testing.T) {
 		assert.Equal(t, arr[i], i, "value in array should be correct at certain id")
 	}
 }
+
+func TestRemove(t *testing.T) {
+	l := list.NewList()
+
+	for i := 0; i < 5; i++ {
+		l.Append(i)
+	}
+
+	l.Remove(2)
+	l.Remove(4)
+
+	arr := l.GetArray()
+	
+	assert.Equal(t, len(arr), 3, "array size should be 3");
+
+	assert.Equal(t, arr[0], 0, "arr[0] should equal 0");
+	assert.Equal(t, arr[1], 2, "arr[0] should equal 0");
+	assert.Equal(t, arr[2], 3, "arr[0] should equal 0");
+}
